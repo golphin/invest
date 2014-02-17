41a140b07fef802f5399b7a5ffb3545234f9630b
Luiz Tsuha <luiz.tsuha@gmail.com>
2014-02-16 21:48:44 -0300

    Projeto Invest - initial load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Invest::Application.config.secret_key_base = '1b060e113bd01a491b35749a011b62f82381d621bfeebcf1568650ca066c4eb1beb7c0aaaca59a200ce9546b201b75cca1893b0f9393620fcb79ee59d980d11a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Invest::Application.config.session_store :cookie_store, key: '_invest_session'
