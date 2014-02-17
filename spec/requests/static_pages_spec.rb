d7e8ee7b2e472c9bb96b60c866fd181f4012b064
Luiz Tsuha <luiz.tsuha@gmail.com>
2014-02-17 18:10:20 -0300

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+     it "should have the content 'Home'" do
+        visit '/static_pages/home'
+        expect(page).to have_content('Home')
+      end
+  
+    it "should have the title'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Invest | Home')
+    end
+     
+  end
+  
+  
+  describe "Help page" do
+   it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    
+   it "should have the title 'Invest | Help'" do
+        visit '/static_pages/help'
+        expect(page).to have_title('Invest | Help')
+   end
+    
+  end
+  
+  describe "About page" do
+   it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+    
+    it "should have the title 'Invest |About'" do
+       visit '/static_pages/about'
+       expect(page).to have_title('Invest | About')
+    end
+    
+  end
+  
+end
+
+
