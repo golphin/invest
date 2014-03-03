dd6a48cde0bdc2386e6425c1d9cf6b707b55c460
Luiz Tsuha <luiz.tsuha@gmail.com>
2014-03-03 11:27:44 -0300

    teste criados

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,6 +54,27 @@ describe User do
       
     end
     
+    describe "when email format is invalid" do
+      it "should be invalid" do
+        addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                       foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+        addresses.each do |invalid_address|
+          @user.email = invalid_address
+          expect(@user).not_to be_valid
+        end
+      end
+    end
+
+    describe "when email format is valid" do
+      it "should be valid" do
+        addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+        addresses.each do |valid_address|
+          @user.email = valid_address
+          expect(@user).to be_valid
+        end
+      end
+    end
+    
     describe "return value of authenticate method" do
       before { @user.save }
       let(:found_user) { User.find_by(email: @user.email) }
@@ -70,5 +91,13 @@ describe User do
       end
     end  
   
-  
+  describe "Verifica se salva email em letras minusculas" do
+     let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    it "should be saved as all lower-case" do
+        @user.email = mixed_case_email
+        @user.save
+        expect(@user.reload.email).to eq mixed_case_email.downcase
+       end
+  end
+   
 end
